5ee42fd881bf4505bdcf8ebb9f23d8c7c4cca7b6
Alejandro Perez <alexroperez4@gmail.com>
2021-02-04 18:24:54 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
 					<p style={{ color: 'var(--clr-neutral-100)' }}>art</p>
 				</a>
 				<div ref={ref} style={{ zIndex: 0, position: 'relative' }}>
-					<div style={{ height: '5vh' }}></div>
+					<div style={{ height: '8vh' }}></div>
 					<div className={'hero-image'}>
 						<motion.img style={{ scale, opacity }} src={skull}></motion.img>
 					</div>
