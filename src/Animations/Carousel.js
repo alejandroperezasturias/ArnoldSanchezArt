8b4fb9182f847293536f6f1ee30ef483fe303422
Alejandro Perez <alexroperez4@gmail.com>
2021-02-08 13:48:26 +0100

    check phone

--- src/Animations/Carousel.js
+++ src/Animations/Carousel.js
@@ -1,15 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useMemo, useState } from 'react';
 import boxingGloves from '../images/boxing.svg';
 import apple from '../images/apple.svg';
 import eye from '../images/eye.svg';
 
-const containerCss = {
-	display: 'flex',
-	justifyContent: 'flex-end',
-	alignItems: 'center',
-	width: '100%',
-	height: '90vh',
-};
 const slideCss = {
 	position: 'relative',
 	left: '500px',
@@ -17,6 +10,13 @@ const slideCss = {
 };
 
 const Carousel = () => {
+	const [upOrDown, setUporDown] = useState('');
+	const [slideNumber, setSlideNumber] = useState(0);
+	const prevUporDown = useRef();
+	const onWheelThrottled = useMemo(() => {
+		return { direcction: upOrDown, times: slideNumber };
+	}, [upOrDown, slideNumber]);
+
 	//  Useref does not cause component to re-render, unlike State
 	const ref = useRef();
 	const ref2 = useRef();
@@ -24,40 +24,76 @@ const Carousel = () => {
 
 	useEffect(() => {
 		window.addEventListener('wheel', wheelslide);
-		window.addEventListener('touchmove', wheelslide);
+		// window.addEventListener('touchmove', touchSlide);
 		return () => {
 			window.removeEventListener('wheel', wheelslide);
-			window.removeEventListener('touchmove', wheelslide);
+			// window.removeEventListener('touchmove', touchSlide);
 		};
-	}, []);
+	});
 
-	function wheelslide(e) {
-		// scrolling downward
-		if (ref === undefined || ref2 === undefined || ref3 === undefined) return;
-		if (e.deltaY > 0) {
-			if (ref.current.style.left === '500px' || ref.current.style.left === '') {
+	useEffect(() => {
+		console.log(onWheelThrottled);
+	}, [onWheelThrottled]);
+
+	useEffect(() => {
+		if (onWheelThrottled.direcction === 'down') {
+			if (ref.current.style.left === '500px') {
 				ref.current.style.left = '50px';
 				ref2.current.style.left = '50px';
 				ref3.current.style.left = '50px';
 			}
-		} else {
+		}
+
+		if (onWheelThrottled.direcction === 'up') {
 			if (ref.current.style.left === '50px') {
 				ref.current.style.left = '500px';
 				ref2.current.style.left = '500px';
 				ref3.current.style.left = '500px';
 			}
 		}
+	}, [onWheelThrottled]);
+
+	function wheelslide(e) {
+		if (e.deltaY > 0) {
+			setUporDown('down');
+		} else {
+			setUporDown('up');
+		}
 	}
+
 	return (
-		<div style={containerCss}>
-			<img ref={ref} style={slideCss} src={eye} alt="eye"></img>
-			<img
-				ref={ref2}
-				style={slideCss}
-				src={boxingGloves}
-				alt="boxingGloves"
-			></img>
-			<img ref={ref3} style={slideCss} src={apple} alt="apple"></img>
+		<div style={{ display: 'flex', flexDirection: 'column' }}>
+			<div className="containerCarousel">
+				<img
+					ref={ref}
+					style={slideCss}
+					src={eye}
+					alt="eye"
+					className="carousel-slide"
+					id={0}
+				></img>
+				<img
+					ref={ref2}
+					style={slideCss}
+					src={boxingGloves}
+					alt="boxingGloves"
+					className="carousel-slide"
+					id={1}
+				></img>
+				<img
+					ref={ref3}
+					style={slideCss}
+					src={apple}
+					alt="apple"
+					className="carousel-slide"
+					id={2}
+				></img>
+			</div>
+			<div>
+				<div style={{ position: 'absolute', top: 0, left: 0 }}>
+					<h1>Previous WAS {prevUporDown.current}</h1>
+				</div>
+			</div>
 		</div>
 	);
 };
